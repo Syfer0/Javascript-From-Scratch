853125105e3c9501b73e1521a0c0e380479b93da
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-01 18:06:09 +0530

    task 2 done

---
+++ Javascript/Exercise practicing loop/Task2.js
@@ -0,0 +1,30 @@
+var day = 'Sunday';
+switch(day) {
+   case 'Monday':
+       console.log('Read a book');
+       break;
+   case 'Tuesday':
+       console.log('Watch a movie');
+       break;
+   case 'Wednesday':
+       console.log('Read a book');
+       break;
+   case 'Thursday':
+       console.log('Play basketball');
+       break;
+   case 'Friday':
+       console.log('Socialize');
+       break;
+   case 'Saturday':
+       console.log('Chill');
+       break;
+   case 'Sunday':
+       console.log('Have barbecue');
+       break;
+   default:
+       //this block will run if no condition matches
+       console.log('There is no such day');
+}
+//tip to do it own if you understand try solve without solution with problem statement 
+
+ 
