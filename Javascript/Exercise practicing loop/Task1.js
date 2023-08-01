ff1e0aeb9e055cfb664e7c2d49edf97b205737c2
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-01 17:33:04 +0530

    condition statement task1 done

---
+++ Javascript/Exercise practicing loop/Task1.js
@@ -0,0 +1,14 @@
+var age = 23;
+if (age >= 65){
+    console.log('You got the pension')
+} 
+else if (age < 65 && age >= 18){
+    console.log('you got salary')
+}
+else if( age <= 18){
+    console.log('you got pocket money ')
+}
+else{
+    console.log("you didn't enter your age")
+    }
+//modify code according to you to play with code 
