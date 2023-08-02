f43aeebc8d12011070c93c7ca2ad788ce38e602d
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-02 11:40:03 +0530

    all done task for while and for looop

---
+++ Javascript/Exercise practicing loop/Exerciser loop condition/task5.js
@@ -0,0 +1,11 @@
+ // console.log(2018)
+// console.log(2019)
+// console.log(2020)
+// console.log(2021)
+// console.log(2022)
+
+var year = 2017;
+while( year < 2023){
+    console.log(year);
+    year++;
+};
