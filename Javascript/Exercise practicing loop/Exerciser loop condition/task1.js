8f2548d318378c917614bf8ec53d4d95ec5c383a
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-02 11:10:03 +0530

    task one Done

---
+++ Javascript/Exercise practicing loop/Exerciser loop condition/task1.js
@@ -0,0 +1,17 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log('Counting completed!')
+
+for(var n = 1; n <=5 ; n++){
+    console.log(n);
+}
+console.log('counting completed!')
+
+
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+};
+console.log('Counting completed!');
