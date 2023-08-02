e30136dec3351efbf43cdcee118c44bb6972b6fa
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-02 11:31:59 +0530

    task 4 done

---
+++ Javascript/Exercise practicing loop/Exerciser loop condition/task4.js
@@ -0,0 +1,14 @@
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+// console.log('Countdown finished!')
+
+var n = 6 ;
+while(n = n - 1){
+    console.log(n);
+
+};
+console.log('Countdown finished!')
+ 
