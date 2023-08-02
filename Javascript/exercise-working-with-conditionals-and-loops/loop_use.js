5a77be51fa2f0b833e6371e0a06d1c18499d16f5
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-02 15:47:54 +0530

    Real life use case code-snipped
    code example real life

---
+++ Javascript/exercise-working-with-conditionals-and-loops/loop_use.js
@@ -0,0 +1,6 @@
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
