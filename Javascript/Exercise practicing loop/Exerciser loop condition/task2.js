682bf0766990918127ce7887bc5c46747f578578
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-02 11:18:35 +0530

    task2 done

---
+++ Javascript/Exercise practicing loop/Exerciser loop condition/task2.js
@@ -0,0 +1,16 @@
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+// console.log('Countdown finished!')
+
+for (var n = 50; n > 5; n--){
+    console.log(n);
+}
+console.log('Countdown finished!'); //always try custom code to know how thinks works!
+
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+};
+console.log('Countdown finished!');
