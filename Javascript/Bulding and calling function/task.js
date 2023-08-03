ad643441fffcc90721d0a4f12f887148e8264146
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-03 09:07:32 +0530

    Task done

---
+++ Javascript/Bulding and calling function/task.js
@@ -0,0 +1,13 @@
+function letterFinder(word, match) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            //check if the current characater, word[i], is equal to the match
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+}
+
+letterFinder("test", "s")
+//all it check second word match in code 
