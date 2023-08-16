67d97310656a0418b2c8518aa18509fdc4de1488
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-16 17:05:43 +0530

    folder change

---
+++ Test/test1.js
@@ -0,0 +1,16 @@
+function testBracketsDynamicAccess() {
+    var dynamicKey;
+    if(Math.random() > 0.5) {
+      dynamicKey = "speed";
+     }else{
+       dynamicKey = "color";
+     }
+  
+      var drone = {
+        speed: 15,
+        color: "orange"
+      }
+  
+      console.log(drone[dynamicKey]);
+  }
+  testBracketsDynamicAccess();
