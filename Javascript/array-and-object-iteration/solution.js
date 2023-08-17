1d42509baacb810a78108c80b83c10e9ee714194
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-17 16:50:29 +0530

    A3 E1 done

---
+++ Javascript/array-and-object-iteration/solution.js
@@ -0,0 +1,28 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+function logDairy(){
+    for (var product of dairy){
+        console.log(product);
+    }
+
+}
+logDairy();
+// Task 2
+const animal = {
+
+    canJump: true
+    
+    };
+    
+    const bird = Object.create(animal);
+    
+    bird.canFly = true;
+    
+    bird.hasFeathers = true;
+function bird(){
+    
+}
+   
+ 
+// Task 3
+
