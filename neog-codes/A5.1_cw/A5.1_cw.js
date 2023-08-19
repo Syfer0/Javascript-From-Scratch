f132ffd3b34fcc3f65df7ac76ed0ce4be4325737
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-19 15:22:15 +0530

    A5.1_cw Done

---
+++ neog-codes/A5.1_cw/A5.1_cw.js
@@ -0,0 +1,44 @@
+console.log("A5.1_cw")
+console.log("---- ---- ----")
+console.log("1.1")
+for (let i= 0; i< 10; i++) { 
+    console.log(i);
+}
+console.log("---- ---- ----")
+console.log("1.2")
+console.log("---- ---- ----")
+let pattern = ""
+for(let i = 1; i <= 4; i++){
+    pattern =  pattern + "1"
+    console.log(pattern)
+}
+console.log("---- ---- ----")
+console.log("1.3")
+console.log("---- ---- ----")
+let Alphabetpatternss =""
+for(let i = 1; i <= 5; i++){
+    Alphabetpatternss =  Alphabetpatternss + "a"
+    console.log(Alphabetpatternss)
+}
+console.log("---- ---- ----")
+console.log("1.4")
+console.log("---- ---- ----")
+let numberpattern = ""
+for (let i = 1; i <= 5; i++){
+    numberpattern = numberpattern + i
+    console.log(numberpattern);
+}
+console.log("---- ---- ----")
+console.log("1.5")
+console.log("---- ---- ----")
+for (i =1; i<=10;i++){
+    result = 2 * i
+    console.log(" 2 * "+ i +" = " + result)
+}
+console.log("---- ---- ----")
+console.log("1.6")
+console.log("---- ---- ----")
+for( i = 1;i<=10;i++){
+    let square= i * i
+    console.log(i +" * "+ i + " = " + square)
+}
