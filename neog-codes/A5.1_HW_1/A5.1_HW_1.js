8f980db4238a9c7d017c3b1d55e0d927c7017d32
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-19 16:01:56 +0530

    A5.1_HW_1 Done

---
+++ neog-codes/A5.1_HW_1/A5.1_HW_1.js
@@ -0,0 +1,40 @@
+console.log("A5.1_HW_1")
+console.log("---- ---- ----")
+console.log("1.1")
+for (let i = 10 ; i <= 20; i++) {
+    console.log(i)
+}
+console.log("---- ---- ----")
+console.log("1.2")
+let  numberpattern = ""
+for(i = 1; i<=4;i++){
+     numberpattern =  numberpattern + "2"
+    console.log( numberpattern )
+}
+console.log("---- ---- ----")
+console.log("1.3")
+let Alphabetpatternss = ""
+for( let i = 1; i<=4;i++){
+    Alphabetpatternss = Alphabetpatternss + "b"
+    console.log(Alphabetpatternss )
+}
+console.log("---- ---- ----")
+console.log("1.4")
+let increse = ""
+for (let i =1;i <= 6;i++){
+    increse = increse + i
+    console.log(increse); // Basically we need to put outside so its Does'nt work how i wnat to 
+}
+console.log("---- ---- ----")
+console.log("1.5")
+for (let i = 1;i <=10;i++){
+    result = 3 * i
+    console.log("3"+ " * " + i +  " = "+ result )
+}
+console.log("---- ---- ----")
+console.log("1.6")
+for(let i = 11; i <= 20;i++){
+    squre = i * i
+    console.log(i +" * "+ i + " = " + squre )
+}
+ 
---
+++ neog-codes/A5.1_HW_1/Readme.md
@@ -0,0 +1,76 @@
+# A5.1_HW_1
+
+1.1: Print Numbers from 10 to 20 using for loops.
+
+```jsx
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+```
+
+1.2: Print Repeating Numbers using for loops
+
+```jsx
+2
+22
+222
+2222
+```
+
+1.3: Print Repeating Characters using for loops.
+
+```jsx
+b
+bb
+bbb
+bbbb
+bbbbb
+```
+
+1.4: Print Number Patterns using for loops.
+
+```jsx
+1
+12
+123
+1234
+12345
+123456
+```
+
+1.5: Print Table of 3 using for loops.
+
+```jsx
+3 * 1 = 3
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+3 * 6 = 18
+3 * 7 = 21
+3 * 8 = 24
+3 * 9 = 27
+3 * 10 = 30
+```
+
+1.6: Print Squares of Numbers using for loops.
+```jsx
+11 * 11 = 121
+12 * 12 = 144
+13 * 13 = 169
+14 * 14 = 196
+15 * 15 = 225
+16 * 16 = 256
+17 * 17 = 289
+18 * 18 = 324
+19 * 19 = 361
+20 * 20 = 400
+```
