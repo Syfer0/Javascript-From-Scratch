fce46af1ca08dce2ece96ae1f98d30732e77acf3
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-24 02:51:40 +0530

    Update A5.4_CW.js

--- Javascript pratice question for Beginner/A5.4_CW/A5.4_CW.js
+++ Javascript pratice question for Beginner/A5.4_CW/A5.4_CW.js
@@ -3,12 +3,23 @@ console.log("---- ---- ----")
 console.log("1.1")
 console.log("---- ---- ----")
 function sumuptoN(n){
-    let sum =0
-    for(let i;i<=n;i++){
+    let sum = 0
+    for(let i=1;i<=n;i++){
         sum = sum + i
     }
     return sum
 }
 console.log(sumuptoN(5))
-
-
+console.log("---- ---- ----")
+console.log("1.2")
+console.log("---- ---- ----")
+function someofoddNumnersuptoN(n){
+    let sum = 0
+    for(let i=0;i<=n;i++){
+        if (i % 2 !== 0){
+            sum = sum + i
+        } 
+    }
+    return sum
+}
+console.log(someofoddNumnersuptoN(7))
