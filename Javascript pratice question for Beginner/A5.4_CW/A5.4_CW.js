2211a04badfc20695479c2c629d723f70eb912ec
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-24 00:04:51 +0530

    Create A5.4_CW.js

---
+++ Javascript pratice question for Beginner/A5.4_CW/A5.4_CW.js
@@ -0,0 +1,14 @@
+console.log("A5.3_CW")
+console.log("---- ---- ----")
+console.log("1.1")
+console.log("---- ---- ----")
+function sumuptoN(n){
+    let sum =0
+    for(let i;i<=n;i++){
+        sum = sum + i
+    }
+    return sum
+}
+console.log(sumuptoN(5))
+
+
