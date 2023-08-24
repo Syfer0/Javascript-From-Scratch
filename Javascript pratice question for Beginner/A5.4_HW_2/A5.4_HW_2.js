9151e3b7d7b8e7b25c0c91c730e92590c0b080c3
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-24 23:43:28 +0530

    docs: added Readme file

--- Javascript pratice question for Beginner/A5.4_HW_2/A5.4_HW_2.js
+++ Javascript pratice question for Beginner/A5.4_HW_2/A5.4_HW_2.js
@@ -1,21 +1,68 @@
-console.log("A5.3_HW_2")
-console.log("---- ---- ----")
+console.log("A5.4_HW_2");
+console.log("Exercise 1")
+console.log("------ ------ ------");
 console.log("1.1")
-console.log("---- ---- ----")
-function averageOfNumbersuptoN (n){
-    let sum = 0
-    for(let i =1;i<=n;i++){
-        sum += i;
-    }
-    return sum / n
+function averageOfNumbersuptoN(n){
+  let sum=0;
+  let divident=0;  
+  for (let i = 1;i<=n;i++){
+  sum+=i;
+  divident+=1;      
+  }
+  console.log("The average of numbers from 1 to 10: ",sum/divident)
 }
-let n = 10;
-const average = averageOfNumbersuptoN(n);
+averageOfNumbersuptoN(10);
 
-console.log(`The average of numbers from 1 to ${n}: ${average}`);
-console.log("---- ---- ----")
+console.log("------ ------ ------");
 console.log("1.2")
-console.log("---- ---- ----")
-function sumOfNumbersGreaterThanSixUpToN (n){
 
+
+function sumOfNumbersGreaterThanSixUpToN(n){
+  let sumSix=0;
+  
+  for(let i =1;i<=n;i++){
+    
+    if(i>6){
+      sumSix+=i;
+    }   
+  }    
+  console.log("Sum of Even numbers : ",sumSix)  
 }
+sumOfNumbersGreaterThanSixUpToN(12);
+
+console.log("------ ------ ------");
+console.log("1.3");
+function sumDivisibleByEightUpToN(n){
+ let sum =0;
+  for(let i=10;i<=n;i++){
+    if(i%8==0){
+      sum+=i;
+    }
+    
+  }
+   console.log(sum) 
+}  
+sumDivisibleByEightUpToN(40)
+console.log("------ ------ ------");
+console.log("1.4");
+function sumEvenAndOddUpToN(n){
+  let even=0;
+  let odd=0;
+for(let i =1;i<=n;i++){
+  if(i%2===0){
+    even+=i
+  }else{
+    odd+=i;
+  }
+
+
+  
+}
+  console.log("Sum of even numbers: ",even);
+  console.log("Sum of Odd numbers: ",odd);
+
+
+  
+}
+sumEvenAndOddUpToN(30)
+
---
+++ Javascript pratice question for Beginner/A5.4_HW_2/Readme.md
@@ -0,0 +1,29 @@
+# A5.4_HW_2
+
+### 1.1:
+
+Write a JavaScript function named `averageOfNumbersuptoN` that takes a number `n` as input and calculates the sum of all numbers from 1 to `n` and returns their average. Take n as 10.
+
+```jsx
+The average of numbers from 1 to 10: 5.5
+```
+
+### 1.2:
+
+Write a JavaScript function named `sumOfNumbersGreaterThanSixUpToN` that takes a number `n` as input and returns the sum of all numbers which are greater than six from 1 to `n`. Take n as 12.
+
+```jsx
+The sum of numbers is: 57
+```
+
+### 1.3:
+
+Write a JavaScript function named `sumDivisibleByEightUpToN` that takes a number `n` as input and prints the sum of all numbers divisible by 8 from 10 to `n`. Take n as 40.
+
+```jsx
+112
+```
+
+### 1.4:
+
+Write a JavaScript function named `sumEvenAndOddUpToN` that takes a number `n` as input and prints the sum of all even numbers and the sum of all odd numbers from 1 to `n`. Take n as 30. Print the output in the following format:
