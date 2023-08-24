a98a2051e38bbadd8a7c78925d76b8b36796801a
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-24 22:20:35 +0530

    Update A5.4_CW.js

--- Javascript pratice question for Beginner/A5.4_CW/A5.4_CW.js
+++ Javascript pratice question for Beginner/A5.4_CW/A5.4_CW.js
@@ -23,3 +23,32 @@ function someofoddNumnersuptoN(n){
     return sum
 }
 console.log(someofoddNumnersuptoN(7))
+console.log("---- ---- ----")
+console.log("1.3")
+console.log("---- ---- ----")
+function sumofNumberDivisibleByThreeAndSevenuptoN(n){
+    let sumofNumberDivisibleByThree = 0
+    let sumofNumberDivisibleBySeven = 0
+    for(let i = 1;i<=n;i++){
+        if (i %3 === 0){
+        sumofNumberDivisibleByThree = sumofNumberDivisibleByThree + i
+    }
+        if( i % 7 === 0){
+            sumofNumberDivisibleBySeven = sumofNumberDivisibleBySeven + i
+        }
+    }
+    console.log(" sum number are divisible by 3:" + " ",sumofNumberDivisibleByThree)
+    console.log("sum number are divisible by 7:" + " ",sumofNumberDivisibleBySeven)
+}
+sumofNumberDivisibleByThreeAndSevenuptoN(21)
+console.log("---- ---- ----")
+console.log("1.4")
+console.log("---- ---- ----")
+function factorial(n){
+    let result = 1
+    for(let i = 1;i<=n;i++){
+        result = result * i
+    }
+    return result 
+}
+console.log(factorial(5))
