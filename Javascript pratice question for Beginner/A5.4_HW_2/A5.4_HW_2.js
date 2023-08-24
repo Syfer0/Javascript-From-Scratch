31ff89ac3df34dd558fa4fedb796e94ba57f9a76
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-24 23:20:56 +0530

    Update :  Created a HW_2

---
+++ Javascript pratice question for Beginner/A5.4_HW_2/A5.4_HW_2.js
@@ -0,0 +1,18 @@
+console.log("A5.3_HW_2")
+console.log("---- ---- ----")
+console.log("1.1")
+console.log("---- ---- ----")
+function averageOfNumbersuptoN (n){
+    let sum = 0
+    for(let i =1;i<=n;i++){
+        sum += i;
+    }
+    return sum / n
+}
+const n = 10;
+const average = averageOfNumbersuptoN(n);
+
+console.log(`The average of numbers from 1 to ${n}: ${average}`);
+console.log("---- ---- ----")
+console.log("1.2")
+console.log("---- ---- ----")
