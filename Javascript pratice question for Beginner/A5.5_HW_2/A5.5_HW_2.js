5bca96013b61b3060afc6324481cd342634a3b09
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-25 23:49:00 +0530

    Update :A5.5_HW_2

---
+++ Javascript pratice question for Beginner/A5.5_HW_2/A5.5_HW_2.js
@@ -0,0 +1,28 @@
+console.log("A5.5_HW_2");
+console.log("Exercise 1")
+console.log("------ ------ ------");
+console.log("1.1")
+let topIndianArtists = ["Arijit Singh", "Neha Kakkar", "Badshah", "Shreya Ghoshal"];
+console.log(topIndianArtists);
+console.log("------ ------ ------");
+console.log("1.2")
+console.log(topIndianArtists[2]);;
+console.log("------ ------ ------");
+console.log("1.3")
+console.log("Number of artists: ", topIndianArtists.length);
+console.log("------ ------ ------");
+console.log("1.4")
+console.log(topIndianArtists[topIndianArtists.length - 1]);
+console.log("------ ------ ------");
+console.log("1.5");
+for (let i = 0; i < topIndianArtists.length; i++) {
+  console.log(topIndianArtists[i]);
+}
+console.log("------ ------ ------");
+console.log("1.6");
+topIndianArtists.push("Amit Trivedi");
+console.log(topIndianArtists);
+
+console.log("------ ------ ------");
+console.log("1.7")
+console.log("Updated number of artists: ", topIndianArtists.length);
---
+++ Javascript pratice question for Beginner/A5.5_HW_2/Readme.md
@@ -0,0 +1,23 @@
+# A5.5_HW_2
+
+### **Top Music Artists**
+
+You have a list of top music artists. Use the given array and perform the following tasks:
+
+```jsx
+const topIndianArtists = ["Arijit Singh", "Neha Kakkar", "Badshah", "Shreya Ghoshal"];
+```
+
+1.1 Print the entire array of artist names.
+
+1.2 Print the third artist's name (index 2) in the array.
+
+1.3 Print the total number of artists in the array.
+
+1.4 Print the name of the last artist in the array.
+
+1.5 Print one artist's name at a time using a for loop.
+
+1.6 Add a new artist to the list. `"Amit Trivedi"` and print the updated array of artists.
+
+1.7 Print the updated total number of artists in the array.
