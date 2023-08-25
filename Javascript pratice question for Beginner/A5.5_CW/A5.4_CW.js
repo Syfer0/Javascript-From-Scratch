73edbecc894880cd7e6b0a25bf18e7a701193c91
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-25 23:29:01 +0530

    update: A5.5_cW

---
+++ Javascript pratice question for Beginner/A5.5_CW/A5.4_CW.js
@@ -0,0 +1,31 @@
+console.log("A5.5_cW");
+console.log("------ ------ ------");
+console.log("1.1")
+console.log("------ ------ ------");
+let cities = ["New york","London","Tokyo"]
+console.log(cities)
+console.log("------ ------ ------");
+console.log("1.2")
+console.log("------ ------ ------");
+console.log(cities[1])
+console.log("------ ------ ------");
+console.log("1.3")
+console.log("------ ------ ------");
+console.log("Number of cites :",cities.length)
+console.log("------ ------ ------");
+console.log("1.4")
+console.log("------ ------ ------");
+console.log(cities[cities.length - 1])
+console.log("------ ------ ------");
+console.log("1.5")
+console.log("------ ------ ------");
+for (let i = 0; i< cities.length; i++)
+console.log(cities[i])
+console.log("------ ------ ------");
+console.log("1.6")
+console.log("------ ------ ------");
+cities.push("paris")
+console.log("1.7")
+console.log("------ ------ ------")
+console.log("updated number of cites :",cities.length)
+console.log(cities)
