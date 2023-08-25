fe91038720855ead0303cbeb0e69f5fc1d22dc06
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-25 23:31:25 +0530

    fixed;isuse with  A5_HW_1 solved

--- Javascript pratice question for Beginner/A5.5_HW_1/A5.5_HW_1.js
+++ Javascript pratice question for Beginner/A5.5_HW_1/A5.5_HW_1.js
@@ -1,5 +1,6 @@
 console.log("A5.5_HW_1")
-const favoriteBooks = ["To Kill a Mockingbird", "Pride and Prejudice", "1984", "The Great Gatsby"];const favoriteBooks = ["To Kill a Mockingbird", "Pride and Prejudice", "1984", "The Great Gatsby"];
 console.log("---- ---- ----")
 console.log("1.1")
 console.log("---- ---- ----")
+const favoriteBooks = ["To Kill a Mockingbird", "Pride and Prejudice", "1984", "The Great Gatsby"]; 
+console.log(favoriteBooks)
