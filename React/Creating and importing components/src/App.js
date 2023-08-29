055615a7d8ccaf46a6d200df097a4ef128b54e0c
Arman <125900320+Syfer0@users.noreply.github.com>
2023-08-29 22:48:51 +0530

    update:all file

---
+++ React/Creating and importing components/src/App.js
@@ -0,0 +1,16 @@
+function Heading() {
+  return (
+    <h1></h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      This is the starting code for "Your first component" ungraded lab
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
